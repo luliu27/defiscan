698bf5a293b6af88d81fd15bd14d4e338b542797
Lu Liu <luliu27@gmail.com>
2022-05-11 21:51:46 -0400

    add gas tracker for polygon

--- gas_tracker.py
+++ gas_tracker.py
@@ -15,6 +15,9 @@ flags.DEFINE_integer("interval", 5, "interval between requests", lower_bound=1)
 config = json.load(open("config.json", "r"))
 eth = Etherscan(config["eth"]["etherscan"])
 ftm_gas_ep = "https://api.ftmscan.com/api?module=gastracker&action=gasoracle&apikey=" + config["ftm"]["ftmscan"]
+poly_gas_ep = (
+    "https://api.polygonscan.com/api?module=gastracker&action=gasoracle&apikey=" + config["polygon"]["polygonscan"]
+)
 
 
 def track_eth_gas():
@@ -27,31 +30,31 @@ def track_eth_gas():
             "fast": gas["FastGasPrice"],
             "propose": gas["ProposeGasPrice"],
             "baseFee": gas["suggestBaseFee"],
-            "ts": datetime.now().isoformat(),
+            "ts": datetime.now().isoformat(timespec="seconds"),
         }
     except:
         logging.warning("etherscan api error")
         return None
 
 
-def track_ftm_gas():
+def track_L1_gas(chain, ep):
     try:
-        resp = requests.get(ftm_gas_ep)
+        resp = requests.get(ep)
         if resp.status_code != 200:
-            logging.warning("ftm gas tracker endpoint failure")
+            logging.warning("L1 gas tracker endpoint failure")
             return None
         gas = json.loads(resp.text)["result"]
         return {
-            "chain": "ftm",
+            "chain": chain,
             "block": int(gas["LastBlock"]),
             "safe": gas["SafeGasPrice"],
             "fast": gas["FastGasPrice"],
             "propose": gas["ProposeGasPrice"],
-            "baseFee": "0.0",
-            "ts": datetime.now().isoformat(),
+            "baseFee": gas["suggestBaseFee"] if "suggestBaseFee" in gas else "0.0",
+            "ts": datetime.now().isoformat(timespec="seconds"),
         }
     except Exception as e:
-        logging.warning("ftm api error: {}", format(e))
+        logging.warning("{} api error: {}".format(chain, e))
         return None
 
 
@@ -70,9 +73,13 @@ def main(_):
             if eth_gas != None:
                 gas.append(eth_gas)
         if "ftm" in chains:
-            ftm_gas = track_ftm_gas()
+            ftm_gas = track_L1_gas("ftm", ftm_gas_ep)
             if ftm_gas != None:
                 gas.append(ftm_gas)
+        if "polygon" in chains:
+            matic_gas = track_L1_gas("matic", poly_gas_ep)
+            if matic_gas != None:
+                gas.append(matic_gas)
         df = pandas.DataFrame.from_records(data=gas)
         print(df)
         sleep(interval)
